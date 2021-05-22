a0ce150617c35be3bf3cda41dc4749c1edc391be
Danny <damaraa96@gmail.com>
2021-05-22 13:56:53 +0700

    Quiz1

---
+++ Quiz1.js
@@ -0,0 +1,21 @@
+// const input = 'gajah'
+// let result = ''
+// for (let i=input.length-1; i >= 0; i--){
+//     result += input[i]
+// }
+
+
+// console.log(result) //hajag
+//pokoknya hasil result itu kebalikan dari input
+
+//===================================================================
+
+function balikkata(input){
+    let result = ''
+    for (let i=input.length-1; i >= 0; i--){
+        result += input[i]
+    } 
+    return result;
+}
+console.log(balikkata('gajah'));
+
