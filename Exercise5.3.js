dd66619b405cd95b2034f13b72205804870d1ea4
Danny <damaraa96@gmail.com>
2021-05-23 23:35:25 +0700

    exercise 5.4

--- Exercise5.3.js
+++ Exercise5.3.js
@@ -1,9 +1,9 @@
 var word3 = 'wow JavaScript is so cool';
 var exampleFirstWord3 = word3.substring(0, 3);
 var secondWord3 = word3.substring(4,14);
-var thirdWord3 = word3.substring(15,16);
-var fourthWord3 = word3.substring(18,19);
-var fifthWord3 = word3.substring(21,24);
+var thirdWord3 = word3.substring(15,17);
+var fourthWord3 = word3.substring(18,20);
+var fifthWord3 = word3.substring(21,25);
 
 console.log('First Word: ' + exampleFirstWord3);
 console.log('Second Word: ' + secondWord3);
---
+++ Exercise5.4.js
@@ -0,0 +1,20 @@
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondword4 = word4.substring(4,14);
+var thirdword4 = word4.substring(15,17);
+var fourthword4 = word4.substring(18,20);
+var fifthword4 = word4.substring(21,25);
+
+var firstWordLength = exampleFirstWord4.length;
+var secondword4Length = secondword4.length ;
+var thirdword4Length = thirdword4.length ;
+var fourthword4Length = fourthword4.length ;
+var fifthword4Length = fifthword4.length ;
+
+
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondword4 + ', with length : ' + secondword4Length);
+console.log('Third Word: ' + thirdword4 + ', with length: ' + thirdword4Length);
+console.log('Fourth Word: ' + fourthword4 + ', with length : ' + fourthword4Length);
+console.log('Fifth Word: ' + fifthword4 + ', with length :' + fifthword4Length);
