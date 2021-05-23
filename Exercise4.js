3447e5cf2873d267243c3fd9b7f2fe324226b4a2
Danny <damaraa96@gmail.com>
2021-05-23 22:38:27 +0700

    Exercise4.js

---
+++ Exercise4.js
@@ -0,0 +1,64 @@
+let tanggal = '20';
+let bulan = 5;
+let tahun = '1996' ;
+
+switch(bulan){
+    case 1:
+    bulan = ' Januari ' ;
+    break;
+
+    case 2:
+    bulan = " Februari ";
+    break;
+
+    case 3:
+    bulan = ' Maret ';
+    break;
+
+    
+    case 4:
+    bulan = ' April ';
+    break;
+
+    
+    case 5:
+    bulan = ' Mei ';
+    break;
+
+    
+    case 6:
+    bulan = ' Juni ';
+    break;
+
+    
+    case 7:
+    bulan = ' Juli ';
+    break;
+
+    
+    case 8:
+    bulan = ' Agustus ';
+    break;
+
+    
+    case 9:
+    bulan = ' September ';
+    break;
+
+    
+    case 10:
+    bulan = ' Oktober ';
+    break;
+
+    
+    case 1:
+    bulan = ' November ';
+    break;
+
+    
+    case 12:
+    bulan = ' Desember ';
+    break;
+
+}
+console.log(tanggal + bulan + tahun );
