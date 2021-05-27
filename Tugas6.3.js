8f525cfd0849784c8f9e4c58bb746a752b1553dd
Danny <damaraa96@gmail.com>
2021-05-27 20:28:26 +0700

    Tugas 6.3

---
+++ Tugas6.3.js
@@ -0,0 +1,23 @@
+for(x=0;x<=100;x++){
+    if(x%2==0){
+        console.log(x + " Genap")
+    }else{
+        console.log(x + " Ganjil")
+    }
+}
+
+for(i=0;i<=100;i+=2)
+if(i%3==0){
+    console.log(i + " kelipatan 3")
+}
+
+for(i=0;i<=100;i+=5)
+if(i%5==0){
+    console.log(i + " kelipatan 5")
+}
+
+for(i=0;i<=100;i+=9){
+    if(i%9==0)
+    console.log(i + " kelipatan 9")
+}
+
