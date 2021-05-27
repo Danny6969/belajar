2d24612e838289e6d940d18f256fe1faf18be6bd
Danny <damaraa96@gmail.com>
2021-05-27 20:45:36 +0700

    Tugas6.3

--- Tugas6.3.js
+++ Tugas6.3.js
@@ -6,18 +6,18 @@ for(x=0;x<=100;x++){
     }
 }
 
-for(i=0;i<=100;i+=2)
-if(i%3==0){
+for(i=1;i<=100;i+=2)
+if(i%3===0){
     console.log(i + " kelipatan 3")
 }
 
-for(i=0;i<=100;i+=5)
-if(i%5==0){
-    console.log(i + " kelipatan 5")
+for(i=1;i<=100;i+=5)
+if(i%6===0){
+    console.log(i + " kelipatan 6")
 }
 
-for(i=0;i<=100;i+=9){
-    if(i%9==0)
-    console.log(i + " kelipatan 9")
+for(i=1;i<=100;i+=9){
+    if(i%10==0)
+    console.log(i + " kelipatan 10")
 }
 
