e986bd46f71658d9407e3ad397850474817e92d2
Danny <damaraa96@gmail.com>
2021-05-27 20:01:52 +0700

    Tugas 6.2

---
+++ Tugas6.2.js
@@ -0,0 +1,13 @@
+let a = ' i love coding'
+
+console.log('LOOPING PERTAMA')
+for (x=0 ; x<=20 ; x++){
+    console.log(x + a)
+}
+
+let b = ' i will become fullstack developer'
+
+console.log('LOOPING KEDUA')
+for (i=20;i>=0;i--){
+    console.log(i + b)
+}
