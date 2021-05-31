ddf3c186763ce94731eaa39a80983c3dcd166320
Danny <damaraa96@gmail.com>
2021-05-31 15:38:55 +0700

    Tugas9.2.js

---
+++ Tugas9.2.js
@@ -0,0 +1,10 @@
+function calculateMultiply(){
+    let result =''
+    result = num1 * num2;
+    return result;
+
+}
+var num1 = 5;
+var num2 = 6;
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
